0a3e1a530fec93d9a2703ffed694fa59503b04f5
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2024-07-16 08:53:15 +0800

    update model to gptq ver.

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@
 BASE_MODEL_URL = "http://ec2-13-229-24-160.ap-southeast-1.compute.amazonaws.com:8000/v1"
 BASE_MODEL_NAME = "MaziyarPanahi/Meta-Llama-3-8B-Instruct-GPTQ"
 FINETUNE_MODEL_URL = "http://43.218.77.178:8000/v1"
-FINETUNE_MODEL_NAME = "Ksgk-fy/maria_v113-fp8-dynamic" # v0.1.3 quantised model
+FINETUNE_MODEL_NAME = "Ksgk-fy/maria_v113_gptq_prompt" # v0.1.3 quantised model
 
 # Some API Keys used to simulate & evaluate above served model
 ANTHROPIC_API_KEY = "YOUR_API_KEY"
